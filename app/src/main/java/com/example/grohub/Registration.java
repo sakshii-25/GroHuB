3b43ec1e179e67c295167b6fa660948853aa0833
Sakshi Chauhan <74263821+sugar-rr@users.noreply.github.com>
2021-05-26 11:50:35 +0530

    Revert "changes in registration activity"

--- app/src/main/java/com/example/grohub/Registration.java
+++ app/src/main/java/com/example/grohub/Registration.java
@@ -1,34 +1,24 @@
 package com.example.grohub;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Registration extends AppCompatActivity {
 
-    EditText regName, regUsername, regEmail, regPhoneno, regPassword;
-    Button regBtn, regtoLoginBtn;
+    TextInputLayout regName,regUsername,regEmail,regPhoneno,regPassword;
+    Button regBtn,regtoLoginBtn;
 
-    DatabaseReference databaseReference;
-    FirebaseAuth firebaseAuth;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
 
     @Override
@@ -47,92 +37,33 @@ public class Registration extends AppCompatActivity {
         regBtn = findViewById(R.id.regBtn);
         regtoLoginBtn = findViewById(R.id.regtoLoginBtn);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
-        firebaseAuth = FirebaseAuth.getInstance();
-
-
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
 
-                String name = regName.getText().toString();
-                String username = regUsername.getText().toString();
-                String email = regEmail.getText().toString();
-                String phoneNo = regPhoneno.getText().toString();
-                String password = regPassword.getText().toString();
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email  = regEmail.getEditText().getText().toString();
+                String phoneNo = regPhoneno.getEditText().getText().toString();
+                String password= regPassword.getEditText().getText().toString();
+  
+                UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
 
-                if (TextUtils.isEmpty(name)) {
-                    regName.setError("Name is required.");
-                    return;
-                }
-
-                if (TextUtils.isEmpty(username)) {
-                    regUsername.setError("UserName is required.");
-                    return;
-                }
-
-                if (TextUtils.isEmpty(email)) {
-                    regEmail.setError("Email is required.");
-                    return;
-                }
-
-                if (TextUtils.isEmpty(phoneNo)) {
-                    regPhoneno.setError("Phone no. is required.");
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password)) {
-                    regPassword.setError("Password is required.");
-                    return;
-                }
-
-
-                if (password.length() < 6) {
-                    regPassword.setError("Password must be >= 6 characters.");
-                    return;
-                }
-
-
-                firebaseAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(Registration.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-
-                                UserHelperClass helperclass = new UserHelperClass(name,username,email,phoneNo,password);
-
-                                FirebaseDatabase.getInstance().getReference("Users")
-
-                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                        .setValue(helperclass).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-
-                                    Toast.makeText(Registration.this,"Registration Successful!",Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(getApplicationContext(),Login.class));
-                                    }
-                                });
-                                }
-                                else {
-                                    Toast.makeText(Registration.this,"Error!"+ task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-
-
-                                }
-
-
-                            }
-                        });
+                reference.child(phoneNo).setValue(helperClass);
             }
         });
 
-       /*  regtoLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
+       regtoLoginBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Registration.this, Homepage.class);
-                startActivity(intent);
+               Intent intent = new Intent(Registration.this,Homepage.class);
+               startActivity(intent);
 
 
-            }
-        });*/
+           }
+       });
+
     }
 }
